b80c573d89d6b28173656b4550077b964378d226
Sou Kubota <kubota.sou@vega.ocn.ne.jp>
2023-10-15 23:45:14 +0900

    ログ表示機能を作成

--- src/components/Molecules/ViewLogTable.tsx
+++ src/components/Molecules/ViewLogTable.tsx
@@ -5,17 +5,40 @@ import { useAuthContext } from 'feature/auth/provider/AuthProvider';
 import { firestoreGet, firestoreSet } from 'lib/firebase/firestore';
 
 import style from 'components/Atoms/List.module.css';
-import viewLogListStyle from 'components/Molecules/AddLogList.module.css';
+import viewLogListStyle from 'components/Molecules/ViewLogList.module.css';
+
+type Props = {
+  log: Log
+};
 
 type Log = {
   date: string;
-  value: any;
+  score: any;
 };
 
 type LogByDate = {
   date: string;
   count: number;
 };
+
+const LogItem = ( { log }: Props ) => {
+  let color = [];
+  for(let i=0; i<4; i++){
+    if(log.score[i].point == 0){ color[i] = "#000"; }
+    if(log.score[i].point > 0){ color[i] = "#00f"; }
+    if(log.score[i].point < 0){ color[i] = "#f00"; }
+  }
+  return (
+    <div className={`${style.listitem} ${viewLogListStyle.viewLogList}`} >
+      <p>
+        1: {log.score[0].player} <span className={viewLogListStyle.score_point} style={{color:color[0]}}>{log.score[0].point}</span><br />
+        2: {log.score[1].player} <span className={viewLogListStyle.score_point} style={{color:color[1]}}>{log.score[1].point}</span><br />
+        3: {log.score[2].player} <span className={viewLogListStyle.score_point} style={{color:color[2]}}>{log.score[2].point}</span><br />
+        4: {log.score[3].player} <span className={viewLogListStyle.score_point} style={{color:color[3]}}>{log.score[3].point}</span>
+      </p>
+    </div>
+  );
+}
 
 export const ViewLogTable: React.FC = () => {
   const navigate = useNavigate();
@@ -29,6 +52,8 @@ export const ViewLogTable: React.FC = () => {
     (async () => {
       try {
         const log = (await firestoreGet('log', uid!)).data()?.log.map((x:any) => JSON.parse(x));
+        setLog(log);
+
         let result = [{date: log[log.length-1].date, count: 1}];
         for (let i = log.length - 2; i >= 0; i--) {
           if (result[result.length-1].date !== log[i].date){
@@ -47,13 +72,26 @@ export const ViewLogTable: React.FC = () => {
       }
     })()
   }, [uid]);
-
+  
   return (
-    <ListGroup>
-      { showDate === "" && logDate.map((item) => (
-          <ListItem onClick={() => { setShowDate(item.date) }}>{`${item.date}(${item.count})`}</ListItem>
-        ))
-      }
-    </ListGroup>
+    <>
+      { showDate && <ListTitle>{showDate}</ListTitle> }
+      <ListGroup>
+        { showDate === "" ?
+            logDate.map((item) => (
+              <ListItem onClick={() => { setShowDate(item.date) }}>{`${item.date}(${item.count})`}</ListItem>
+            ))
+          :
+          log.filter((item) => item.date === showDate).map((item) => (
+            <LogItem log={item as Log} />
+          ))
+        }
+      </ListGroup>
+      { showDate && (
+        <ListGroup>
+          <ListItem onClick={() => setShowDate("")}>戻る</ListItem>
+        </ListGroup>
+      )}
+    </>
   );
 };
