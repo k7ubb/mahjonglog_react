fb564437b69168663290a687639ffe33be4af431
k7ubb <kubota.sou@vega.ocn.ne.jp>
2024-03-01 00:40:12 +0900

    AccountPageにローディングを追加

--- src/components/Pages/AccountPage.tsx
+++ src/components/Pages/AccountPage.tsx
@@ -2,21 +2,28 @@ import { Link } from 'react-router-dom';
 import { fireauthLogout } from 'lib/fireauth';
 import { useAuthContext } from 'feature/auth/provider/AuthProvider';
 import { AppTemplate } from 'components/Template/AppTemplate';
+import { LoadingSpin } from 'components/Atoms/LoadingSpin';
 import { AccountProfile } from 'components/Molecules/AccountProfile';
 
 export const AccountPage = () => {
-  const { accountId } = useAuthContext();
+  const { loading, userId } = useAuthContext();
 
-  return (
-    <AppTemplate>
-      {accountId? <>
-        <div style={{ height: "16px" }} />
-        <AccountProfile />
-        <button onClick={() => fireauthLogout()}>Logout</button>
-      </> : <>
-        <Link to='/app/account/signup'>Signup</Link> <br />
-        <Link to='/app/account/login'>Login</Link><br />
-      </>}
-    </AppTemplate>
-  );
+  if (loading) {
+    return <LoadingSpin width="100%" height="100%" />;
+  }
+  
+  else {
+    return (
+      <AppTemplate>
+        {userId? <>
+          <div style={{ height: "16px" }} />
+          <AccountProfile />
+          <button onClick={() => fireauthLogout()}>Logout</button>
+        </> : <>
+          <Link to='/app/account/signup'>Signup</Link> <br />
+          <Link to='/app/account/login'>Login</Link><br />
+        </>}
+      </AppTemplate>
+    );
+        }
 };
