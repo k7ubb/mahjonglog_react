1401b9179b56a3ace7ee4187faa139833f641356
k7ubb <kubota.sou@vega.ocn.ne.jp>
2024-03-01 12:43:25 +0900

    同じアカウントIDを使用できないように

--- src/lib/fireauth.ts
+++ src/lib/fireauth.ts
@@ -1,11 +1,17 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { FirebaseApp } from 'lib/firebase';
-import { firestoreSet } from 'lib/firestore';
+import { firestoreGets, firestoreSet } from 'lib/firestore';
 
+/* eslint-disable */
 const app = FirebaseApp;
-console.log(app);
+/* eslint-disable */
 
 export const fireauthSignup = async (email: string, password: string, accountId: string, accountName: string) => {
+  if (Object.keys(await firestoreGets('user', [
+    { key: 'accountId', operator: '==', value: accountId },
+  ])).length ) {
+    throw new Error("このアカウントIDは使われています");
+  }
   const userCredential = await createUserWithEmailAndPassword(getAuth(), email, password);
   await firestoreSet("user", userCredential.user.uid, {
     email,
